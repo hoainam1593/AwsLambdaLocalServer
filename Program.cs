419d3c636c6eab50952c81108e4e1f786b134655
Nguyễn Hoài Nam <nguyenhoainam@hoppergames.io>
2023-06-07 14:43:33 +0700

    add project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AwsLambdaLocalServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
